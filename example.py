99f284be5110665307e8c7deb7740762038c3607
Walker Davis <walker@wkeithdavis.com>
2022-09-12 18:17:37 -0400

    Updated deg2rad function in example

--- example.py
+++ example.py
@@ -4,20 +4,20 @@ from objects import Box, Camera, Plane
 import numpy as np
 import renderer
 
-def rad2deg(val):
-    return val / np.pi * 180.0
+def deg2rad(val):
+    return val / 180.0 * np.pi
 
 camera_translation = np.array([0.0, 0.0, 0.0], dtype="float64")
 camera_rotation = np.array([0.0, 0.0, 0.0], dtype="float64")
 camera = Camera(camera_translation, camera_rotation, 5)
 
 box_translation = np.array([0.0, 0.0, 0.0])
-box_rotation = rad2deg(np.array([30.0, 0.0, 0.0]))
+box_rotation = deg2rad(np.array([15.0, 0.0, 0.0]))
 box_scale = np.array([1.0, 1.0, 1.0])
 box = Box(box_translation, box_rotation, box_scale)
 
 plane_translation = np.array([0.0, -0.5, 0.0])
-plane_rotation = rad2deg(np.array([30.0, 0.0, 0.0]))
+plane_rotation = deg2rad(np.array([15.0, 0.0, 0.0]))
 plane_scale = np.array([3.0, 1.0, 3.0])
 plane = Plane(plane_translation, plane_rotation, plane_scale)
 
